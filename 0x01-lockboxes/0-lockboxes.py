10fa6cf80302ef4e152f2927a65ad508702f2441
iyasu2 <iyasuforreal@gmail.com>
2024-01-10 21:14:34 +0300

    s

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    num_boxes = len(boxes)
+    unlocked = [False] * num_boxes
+    unlocked[0] = True
+
+    stack = [0]  # Start with the first box
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key < num_boxes and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked)
